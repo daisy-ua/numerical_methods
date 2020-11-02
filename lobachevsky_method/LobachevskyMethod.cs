5b99218981cbb3086656e617c515f1312a22f027
madcassln <eightnum.81@gmail.com>
2020-11-03 00:05:06 +0200

    feat: added bounds obtaining

--- lobachevsky_method/LobachevskyMethod.cs
+++ lobachevsky_method/LobachevskyMethod.cs
@@ -1,14 +1,20 @@
 using System;
+using System.Linq;
 
 namespace lobachevsky_method
 {
     class LobachevskyMethod
     {
-        public static double[] polynomial = { -278, 747, 625, -966, -207, 275, -4, 5 };
+        public static double[] polynomial = { -278, 747, 625, -966, -207, 275, -4, -5 };
 
         public static double epsilon = 1e-7;
 
         public static int maxNumberPositiveRoots;
+
+        public static double upperPositiveBound;
+        public static double lowerPositiveBound;
+        public static double upperNegativeBound;
+        public static double lowerNegativeBound;
 
         static LobachevskyMethod()
         {
@@ -16,7 +22,7 @@ namespace lobachevsky_method
             for (int i = 0; i < polynomial.Length; i++)
             {
                 if (Math.Sign(polynomial[i]) == 0) continue;
-                else if(Math.Sign(polynomial[i]) != sign)
+                else if (Math.Sign(polynomial[i]) != sign)
                 {
                     sign *= -1;
                     signChange += 1;
@@ -31,8 +37,8 @@ namespace lobachevsky_method
             double[] coeffs = (double[])polynomial.Clone();
             double[] prevStateCoeffs = new double[coeffs.Length];
             int counter = 0;
-            
-            do 
+
+            do
             {
                 Array.Copy(Minimize(coeffs), prevStateCoeffs, coeffs.Length);
                 coeffs = GetCoeffs(prevStateCoeffs);
@@ -40,6 +46,18 @@ namespace lobachevsky_method
             } while (!IsFinalCoeffsObtained(prevStateCoeffs, coeffs));
 
             return GetRoots(coeffs, counter);
+        }
+
+        private static double ConvertCoeffsToPolynomial(double x)
+        {
+            double function = 0;
+
+            for (int i = 0; i < polynomial.Length; i++)
+            {
+                function += polynomial[i] * Math.Pow(x, polynomial.Length - 1 - i);
+            }
+
+            return function;
         }
 
         private static bool IsFinalCoeffsObtained(double[] prev, double[] curr)
@@ -57,7 +75,7 @@ namespace lobachevsky_method
         {
             double coeff = 50;
 
-            for  (int i = 0; i < coeffs.Length; i++)
+            for (int i = 0; i < coeffs.Length; i++)
             {
                 coeffs[i] /= coeff;
             }
@@ -106,15 +124,80 @@ namespace lobachevsky_method
             return roots;
         }
 
-        public static int SetSign(double x)
+        private static int SetSign(double x)
         {
             double sum = 0;
             for (int i = 0; i < polynomial.Length; i++)
             {
-                sum += polynomial[i] * Math.Pow(x, polynomial.Length -1 - i);
+                sum += polynomial[i] * Math.Pow(x, polynomial.Length - 1 - i);
             }
 
-            return (Math.Abs(sum) < maxNumberPositiveRoots) ? 1 : -1; 
-        } 
+            return (Math.Abs(sum) < maxNumberPositiveRoots) ? 1 : -1;
+        }
+
+        private static void SetBounds()
+        {
+            double[] lowerPositiveCoeffs = new double[polynomial.Length];
+            double[] upperNegativeCoefss = new double[polynomial.Length];
+            double[] lowerNegativeCoeffs = (double[])polynomial.Clone();
+
+            for (int i = 0; i < polynomial.Length; i++)
+            {
+                lowerPositiveCoeffs[polynomial.Length - 1 - i] = polynomial[i];
+
+                if ((polynomial.Length - 1 - i) % 2 != 0)
+                {
+                    upperNegativeCoefss[polynomial.Length - 1 - i] = -polynomial[i];
+                    lowerNegativeCoeffs[i] *= -1;
+                }
+                else 
+                {
+                    upperNegativeCoefss[polynomial.Length - 1 - i] = polynomial[i];
+                }
+            }
+
+            upperPositiveBound = GetUpperBoundOfPositiveRoots(polynomial);
+            lowerNegativeBound = -GetUpperBoundOfPositiveRoots(lowerNegativeCoeffs);
+
+            try
+            {
+                lowerPositiveBound = 1 / GetUpperBoundOfPositiveRoots(lowerPositiveCoeffs);
+            }
+            catch(System.DivideByZeroException)
+            {
+                lowerPositiveBound = 0;
+            }
+
+            try
+            {
+                upperNegativeBound = -1 / GetUpperBoundOfPositiveRoots(upperNegativeCoefss);
+            }
+            catch(System.DivideByZeroException)
+            {
+                upperNegativeBound = 0;
+            }
+        }
+
+        private static double GetUpperBoundOfPositiveRoots(double[] coeffs)
+        {
+            int firstNegativeIndex = -1;
+            double maxAbsNegative = 0;
+
+            for (int i = 0; i < coeffs.Length; i++)
+            {
+                if (coeffs[i] < 0)
+                {
+                    if (firstNegativeIndex == -1)
+                        firstNegativeIndex = i + 1;
+
+                    if (Math.Abs(coeffs[i]) > maxAbsNegative)
+                        maxAbsNegative = Math.Abs(coeffs[i]);
+                }
+            }
+
+            if (firstNegativeIndex == -1) return 0;
+
+            return 1 + Math.Pow(maxAbsNegative / Math.Abs(coeffs[0]), firstNegativeIndex);
+        }
     }
 }
