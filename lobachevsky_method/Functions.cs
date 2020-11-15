d75965901cd150b7b05e7a84b276fada540d01f9
madcassln <eightnum.81@gmail.com>
2020-11-15 17:23:16 +0200

    feat: added polynomial

--- lobachevsky_method/Functions.cs
+++ lobachevsky_method/Functions.cs
@@ -10,7 +10,12 @@ public static class Functions
 
     public static double f2 (double x) 
     {
-        return Math.Exp(x) + Math.Sin(x) - 10* x - 
-            Math.Pow(x, 10) - Math.Exp(Math.Cos(x));
+        return Math.Exp(x) + Math.Sin(x) - 10* x + 
+            Math.Pow(x, 10) + Math.Exp(Math.Cos(x));
+    }
+
+    public static double f3 (double x)
+    {
+        return -278 * Math.Pow(x, 7) + 747 * Math.Pow(x, 6) + 625 * Math.Pow(x, 5) -966 * Math.Pow(x, 4) -207 * Math.Pow(x, 3) + 275 * Math.Pow(x, 2) -4 * x -5; 
     }
 }
